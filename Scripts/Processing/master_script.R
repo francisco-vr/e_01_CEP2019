5d3fcd3e0903013216e1271f9586445dab393794
GNU-Suri <gnu-suri@GNUSuri.GNU-Suri>
2020-07-10 00:13:43 -0400

    se agrega master script. se completará más adelante.

---
+++ Scripts/Processing/master_script.R
@@ -0,0 +1,22 @@
+# [Entrega N°2]
+# ELECTIVO: CIENCIA ABIERTA Y SOFTWARE LIBRE.
+# MAGÍSTER EN CCSS UNIVERSIDAD DE CHILE - 2020 
+
+# Estudiante: Francisco Villarroel Riquelme
+
+##  MASTER SCRIPT ##
+
+# carga de todos los paquetes
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, summarytools, psych)
+
+#Parte uno: carga de base de datos
+
+# Parte dos: Limpieza de base de datos
+
+# Parte tres: carga de base limpia
+
+# Parte cuatro: tablas de anexos y resultados preeliminares
+
+# Parte cinco: gráficos para analizar
+
