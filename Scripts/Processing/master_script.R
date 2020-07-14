ece73e4911da4154794726bd149bea2bdf106618
Cristóbal Moya <cristobalmoya@gmail.com>
2020-07-14 12:08:33 -0400

    Solucionar uso con master script

--- Scripts/Processing/master_script.R
+++ Scripts/Processing/master_script.R
@@ -18,27 +18,27 @@ pacman::p_load(tidyverse,
 
 #Parte 1: carga y recorte de base de datos
 # Expulsa archivo de datos para editar
-source("Scripts/Processing/carga-base.R")
+source("Scripts/Processing/1carga-base.R")
 
 # Parte 2: Limpieza de base de datos
 # recodifica y agrupa. expulsa base de datos lista para analizar
 
-source("Scripts/Processing/preparacion-base.R")
+source("Scripts/Processing/2preparacion-base.R")
 
 # Parte 3: Construcción de resultados - Tablas
 # realiza tablas descriptivas y cruzadas de variables seleccionadas. expulsa objeto con todas las tablas
 
-source("Scripts/Analysis/Construccion-tablas.R")
+source("Scripts/Analysis/3Construccion-tablas.R")
 
 # Parte 4: Construcción de resultados - Gráficos
 # Realiza gráficos por cada variable analizada. Expulsa imagenes por cada gráfico y un objeto con todos
 
-source("Scripts/Analysis/Construccion-graficos.R")
+source("Scripts/Analysis/4Construccion-graficos.R")
 
 # Parte 5: Crea reporte de investigación
 # En base a los resultados genera reporte reproducible
 
-rmarkdown::render('reporte_reproducible.Rmd')
+rmarkdown::render('reporte2.Rmd')
 
 # Limpiar entorno de trabajo
 rm(list=ls())
